d918dc81c32915c59d08c3a2b15b5bfb494b0ba8
SyntaxBugg <zarbotbekovabbosbek@gmail.com>
2024-09-17 11:23:41 +0500

    ruqirement install

--- testdrf/api/v1/serializers.py
+++ testdrf/api/v1/serializers.py
@@ -1,3 +1,5 @@
+from drf_spectacular.utils import extend_schema_field
+
 from ...models import Task
 from rest_framework import serializers
 from accounts.api.v1.serializers import AuthorSerializer
@@ -7,6 +9,10 @@ from accounts.api.v1.serializers import AuthorSerializer
 
 class TaskSerializer(serializers.ModelSerializer):
     user = AuthorSerializer(read_only=True)
+
+    @extend_schema_field(serializers.ChoiceField(choices=Task.STATUS_CHOICES))
+    def get_status(self, obj):
+        return obj.status
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'status', 'due_date','created_at','update_at','user']
